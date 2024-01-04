5ab08c1b792bfa354dba5b718723568f22fcc135
Henry Sotelo <empresariosotelo@hotmail.com>
2024-01-04 23:00:03 +0000

    Bloque VII - Servicios Externos - Service Enhancement OK

--- srv/sapbackend-exit.js
+++ srv/sapbackend-exit.js
@@ -1,9 +1,10 @@
 const cds = require("@sap/cds");
+const { constants } = require("buffer");
 
-module.exports = cds.service.impl(async function(){
+/*module.exports = cds.service.impl(async function(srv){
     const { Incidents } = this.entities;
     const sapbackend = await cds.connect.to("sapbackend");
-    this.on("READ", Incidents, async (req) => {
+    srv.on("READ", Incidents, async (req) => {
      return await sapbackend.tx(req).send({
             query : req.query,
             headers : {
@@ -12,3 +13,36 @@ module.exports = cds.service.impl(async function(){
         });
     } );
 });
+*/
+
+module.exports = async (srv) => {
+    const sapbackend = await cds.connect.to("sapbackend");
+    const { Incidents } = srv.entities;
+
+    srv.on(["READ"], Incidents, async (req) => {
+        let IncidentsQuery = SELECT.from(req.query.SELECT.from).limit(req.query.SELECT.limit);
+
+        if (req.query.SELECT.where) {
+            IncidentsQuery.where(req.query.SELECT.where);
+        }
+
+        if (req.query.SELECT.orderBy) {
+            IncidentsQuery.orderBy(req.query.SELECT.orderBy);
+        }
+
+        let incident = await sapbackend.tx(req).send({
+            query: IncidentsQuery,
+            headers : {
+                Authorization : "Basic c2FwdWk1OnNhcHVpNQ=="
+            }
+        });
+
+        let incidents = [];
+        if(Array.isArray(incidents)){
+            incidents = incident;
+        } else{
+            incidents[0] = incident;
+        }
+        return incidents;
+    });
+};
